6cb53ac9c49e93d5126aad2dd5f9a023e87aaf69
vikassax1610 <vikassaxena816@gmail.com>
2025-04-04 16:26:40 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="bg-[url(/hero/hero.jpg)] bg-cover bg-bottom h-full text-white">
+      
+        <h1 className="text-5xl text-red-500 font-helveticaRoundedBold">
+          Hello world
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
